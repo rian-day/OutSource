23de9ceee52f6f39b5936b505926260dc08d5871
BaBallover <1352679392@qq.com>
2017-07-16 21:02:57 +0800

    zh7-19    21

--- spring-boot-waibao/src/main/java/com/hyh/controller/ViewController.java
+++ spring-boot-waibao/src/main/java/com/hyh/controller/ViewController.java
@@ -44,31 +44,31 @@ public class ViewController {
 	public String managebbs(){
 		return "manage-bbs";
 	}
-	@RequestMapping("manage-adduser.html")
+	@RequestMapping("/manage-adduser.html")
 	public String manageadduser(){
-		return "";
+		return "manage-adduser";
 	}
-	@RequestMapping("manage-index.html")
+	@RequestMapping("/manage-index.html")
 	public String manageindex(){
 		return "manage-index";
 	}
-	@RequestMapping("manage-info.html")
+	@RequestMapping("/manage-info.html")
 	public String manageinfo(){
 		return "manage-info";
 	}
-	@RequestMapping("manage-pro.html")
+	@RequestMapping("/manage-pro.html")
 	public String managepro(){
 		return "manage-pro";
 	}
-	@RequestMapping("manage-subjectlist.html")
+	@RequestMapping("/manage-subjectlist.html")
 	public String managesubjectlist(){
 		return "manage-subjectlist";
 	}
-	@RequestMapping("manage-tplist.html")
+	@RequestMapping("/manage-tplist.html")
 	public String managetplist(){
 		return "manage-tplist";
 	}
-	@RequestMapping("manage-userlist.html")
+	@RequestMapping("/manage-userlist.html")
 	public String manageuserlist(){
 		return "manage-userlist";
 	}
