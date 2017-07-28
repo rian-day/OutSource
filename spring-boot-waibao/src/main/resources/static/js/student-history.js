102b7363c90beb16fa7282599ff02ebaa17db038
cx49085 <1140746132@qq.com>
2017-07-28 20:46:41 +0800

    cx   7/28/20:46 分页修改+图片路径修改

--- spring-boot-waibao/src/main/resources/static/js/student-history.js
+++ spring-boot-waibao/src/main/resources/static/js/student-history.js
@@ -26,24 +26,43 @@ $(".chakan").on("click", function() {
 })
 
 
-var pp = 0;
-window.onload=function p(){
-    pp = ($(".hidespan:last").text()) * 1;
-    $("#page").Page({
-        totalPages: pp,//分页总数
-        liNums: 7,//分页的数字按钮数(建议取奇数)
-        activeClass: 'activP', //active 类样式定义
-        callBack : function(page){            
-            var url = "selectHistory.do";
-            var args = {
-                "page":page
-            }
-            $.post(url, args, function(data) {
-                /*optional stuff to do after success */
-            });
-        }
-    });
+//分页
+var ye = 0; //当前页数
+var limit = 5; //分页的数字按钮数(建议取奇数)
+var pp = ($(".hidespan:last").text()) * 1;
+var www = window.document.location.href;
+var myurl = www.substring(0, www.indexOf('?'));
+
+//截取地址
+function getUrlParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+    var r = window.location.search.substr(1).match(reg); //匹配目标参数
+    if (r != null) return unescape(r[2]);
+    return null; //返回参数值
 }
+ye = getUrlParam("page");
+if(ye == null || ye ==""){
+    ye = 1;
+}
+//获得总页数
+pp = ($(".hidespan:last").text()) * 1;
+$("#page").Page({
+    totalPages: pp, //分页总数
+    liNums: limit, //分页的数字按钮数(建议取奇数)
+    activeClass: 'activP', //active 类样式定义
+    callBack: function(page) {
+        window.location.href = myurl + "?page=" + page + "";
+    }
+})
+
+//分页请求页面
+var url = "selectHistory.do";
+var args = {
+    "page":ye - 1
+}
+$.post(url,args,function(data){
+
+})
 
 $(".release-content .glyphicon-remove").click(function() {
     $(".release").css("-webkit-animation-name","bounceoutL");
